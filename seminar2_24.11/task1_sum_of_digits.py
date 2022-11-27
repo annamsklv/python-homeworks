ef0cf987ff538f1a3255489bbc3d358a2b22d394
anna.msklv <anna.msklv@yandex.ru>
2022-11-27 23:04:43 +0300

    task1_final

---
+++ seminar2_24.11/task1_sum_of_digits.py
@@ -0,0 +1,31 @@
+# ; 1 - Напишите программу, которая принимает на вход вещественное число и показывает
+# ; сумму его цифр. Учтите, что числа могут быть отрицательными
+
+# ; Пример:
+
+# ; 67.82 -> 23
+# ; (-0.56) -> 11
+
+
+def get_int(float_):
+    while round(float_, 9) % 10 != 0:
+        float_ = round(float_, 9) * 10
+    new_int = int(float_)
+    return new_int // 10
+
+
+def sum_of_digits(number):
+    sum = 0
+    while number > 0:
+        sum = sum + (number % 10)
+        number = number//10
+    return sum
+
+
+try:
+    n = float(input('введите вещественное число:\n'))
+    num = abs(n)
+    print(f'число по модулю без запятой:{get_int(num)}')
+    print(f'сумма цифр в числе: {sum_of_digits(get_int(num))}')
+except ValueError:
+    print('вы ввели не число')
